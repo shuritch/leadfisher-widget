9433696ae5ad9f5b5d37cd26b99b516904349f8a
sashapop10 <sashapop10@yandex.ru>
2023-03-10 15:37:47 +0300

    Draggable target class init

--- src/features/draggable/resize.js
+++ src/features/draggable/resize.js
@@ -8,10 +8,11 @@ export default (options = {}) => {
   let { proxy, save } = options;
   if (typeof proxy === 'string') proxy = document.querySelector(proxy);
 
+  const drag = direction => event => {};
+
   return target => {
     if (!target) error(404);
     const { clear, push } = new EStore();
-    const { setItem, getItem } = new LStore();
     const { selector, getCoordinates, getRect } = $(target);
     const STORE_TAG = selector('[RESIZE]');
 
@@ -19,6 +20,23 @@ export default (options = {}) => {
       const { height, width } = getRect();
       const { x, y } = getCoordinates();
       return { x, y, height, width };
+    };
+
+    const onGrab = event => {
+      const objectInit = getStats();
+      const initCoords = { x: event.pageX, y: event.pageY };
+      const target = $(event.target);
+      const direction = target.getProp('leadfisher-direction');
+      if (direction.includes('self')) target.setProp('leadfisher-move-lock', 'lock');
+      const init = { ...objectInit, ...initCoords, target, direction };
+    };
+
+    const onGrabStop = () => {
+      const { x, y, height, width } = getStats();
+      const { x: initX, y: initY } = init;
+      const { x: targetX, y: targetY } = getCoordinates();
+      const direction = target.getProp('leadfisher-direction');
+      target.removeProp('leadfisher-move-lock');
     };
 
     const events = [];
--- src/features/draggable/target.js
+++ src/features/draggable/target.js
@@ -1,18 +1,29 @@
 import { chain } from '$shared/decorators';
 
+// ? Moveable & Resizable & Move / Resizable
+
 class Draggable {
-  #chain = chain(this);
-  #isMoving = false;
-  #x = 0;
-  #y = 0;
-  #directions = ['x', 'y'];
-  #borders = { s: 0, n: 0, w: 0, e: 0 };
+  /**@protected*/ x = 0;
+  /**@protected*/ y = 0;
+  /**@protected*/ borders = { s: 0, n: 0, w: 0, e: 0 };
+  /**@protected*/ chain = chain(this);
 
   constructor(options) {
+    if (options.borders) this.borders = options.borders;
+    if (options.init?.x) this.x = options.init.x;
+    if (options.init?.x) this.y = options.init.y;
+  }
+
+  dump = () => ({ right: `${this.x}px`, bottom: `${this.y}px`, position: 'absolute' });
+}
+
+class Moveable extends Draggable {
+  #isMoving = false;
+  #directions = ['x', 'y'];
+
+  constructor(options) {
+    super(options);
     if (options.directions) this.#directions = options.directions;
-    if (options.borders) this.#borders = options.borders;
-    if (options.init?.x) this.#x = options.init.x;
-    if (options.init?.x) this.#y = options.init.y;
   }
 
   isMoving = state => {
@@ -21,16 +32,26 @@ class Draggable {
     return state;
   };
 
-  dump = () => ({ right: `${this.#x}px`, bottom: `${this.#y}px`, position: 'absolute' });
-
-  move = this.#chain(({ x = this.#x, y = this.#y, height, width }) => {
+  move = this.chain(({ x = this.x, y = this.y, height, width }) => {
     const { clientHeight, clientWidth } = document.documentElement;
-    const maxX = clientWidth - width - this.#borders.w;
-    const maxY = clientHeight - height - this.#borders.n;
+    const maxX = clientWidth - width - this.borders.w;
+    const maxY = clientHeight - height - this.borders.n;
 
-    if (this.#directions.includes('x')) this.#x = Math.min(Math.max(x, this.#borders.e), maxX);
-    if (this.#directions.includes('y')) this.#y = Math.min(Math.max(y, this.#borders.s), maxY);
+    if (this.#directions.includes('x')) this.x = Math.min(Math.max(x, this.borders.e), maxX);
+    if (this.#directions.includes('y')) this.y = Math.min(Math.max(y, this.borders.s), maxY);
   });
 }
 
-export default Draggable;
+class Resizable extends Draggable {
+  #init = null;
+
+  constructor(options) {
+    super(options);
+  }
+
+  grabStart = init => {
+    this.#init = init;
+  };
+}
+
+export default Moveable;
